3f218af15a402f53e12a1101a51acc9272f93ee7
lcarpentie <>
2022-01-31 16:39:41 +0100

    comme dirait Marianne : youpi !

--- projet/main.cpp
+++ projet/main.cpp
@@ -6,87 +6,86 @@
 using std::cout;
 using std::endl;
 
-/** Méthodes de test */
 
-
-/** Main */
 int main() {
 
-  char s[] = {"Hello"}; 
+  char s[] = {"Hello"};
   char r[] = {'S','a','l','u','t','\0'};
   char t[] = {"Bonjour"};
+  string h;
+  h = 'z';
 
-  // Test des constructeurs
-  cout << "Test du constructeur par défaut :" <<endl;
+
+
+  //TESTING CONSTRUCTORS
+  cout << "Test of constructeur par défaut :" <<endl;
   string b;
   //cout << "b :"<< b.c_str()<< endl;
 
-  cout << "Test du constructor from c-string:" <<endl;
+  cout << "Test of constructor from c-string:" <<endl;
   string d (s);
   cout<<"d : " <<d.c_str()<<endl;
 
   string e (d);
-  cout << "Test du copy constructor :" <<endl;
+  cout << "Test of copy constructor :" <<endl;
   cout<<"e :"<<e.c_str()<<endl;
 
 
-  //Test des fonctions
 
-    cout << "The size of the string is : " << d.size() << " bytes." << endl;    //test of size()
-    cout << "The length of the string is : " << d.length() << " bytes." << endl;    //test of size()
-    cout << "Is the string d empty ? " << d.empty() << endl;                    //test of empty()
-    d.reserve(20);
-    cout<<"Test of reserve:"<<endl;
-    cout<<(d.capacity()==20)<<std::endl;
-    d.clear();
-    cout<<"Test of clear :"<<endl;
-    cout << "d: " <<d.c_str() << endl;
-    cout<< "Test of maxsize :"<<endl;
-    cout<< d.maxsize()<< endl;
-    cout<< "Test of resize : "<< endl;
-    e.resize(8, 'O');
-    cout<<e.c_str()<<endl;
+  //TESTING METHODS
+  cout << "The size of the string d is : " << d.size() << " bytes." << endl;
+  cout << "The length of the string d is : " << d.length() << " bytes." << endl;
+  cout << "Knowing that 1=true and 0=false is the string d empty ? " << d.empty() << endl;
 
-    string h;
-    h = 'z';
-    cout << "Test de c-string :"<<endl;
-    cout << h.c_str() << endl;
+  d.reserve(20);
+  cout<<"Knowing that we just requested a change in capacity, test of reserve:"<<endl;
+  cout<<(d.capacity()==20)<<std::endl;
 
+  d.clear();
+  cout<<"Knowing that we just cleared d, test of clear :"<<endl;
+  cout << "d: " <<d.c_str() << endl;
 
+  cout<< "Test of maxsize :"<<endl;
+  cout<< d.maxsize()<< endl;
 
-    /***Test operator + (const string&,const char*)***/
-    std::cout<<"Test of operator + (const string&,const char*)"<<std::endl;
-    std::string w("Hello");
-    const char* y = ", bonjour\0";
-    cout << w+y << endl;
+  cout<< "Test of resize : "<< endl;
+  e.resize(8, 'O');
+  cout<<e.c_str()<<endl;
 
-    /***Test operator + (const string&, const string&)***/
-    std::cout<<"Test operator + (const string&, const string&)"<<std::endl;
-    std::cout <<w+w<< std::endl;
-
-    /***Test operator + (const string&, char)***/
-    cout<<"Test operator + (const string&, char)"<<endl;
-    char c = 'O' ;
-    cout<<w+c<<endl;
+  cout << "Test de c-string :"<<endl;
+  cout << h.c_str() << endl;
 
 
-    /***Test operator = (const char*)***/
-    std::cout<<"Test of operator = (const char*)"<<std::endl;
-    char const * k = "Hello world";
-	  string K = k;
-	  std::cout<<K.c_str()<<std::endl;
 
-    /***Test operator=(char c)***/
-    std::cout<<"Test of operator=(char c)"<<std::endl;
-    K = c;
-    std::cout<<K.c_str()<<std::endl;
+  //TESTING OPERATORS +
+  std::cout<<"Test of operator + (const string&,const char*)"<<std::endl;
+  std::string w("Hello");
+  const char* y = ", bonjour\0";
+  cout << w+y << endl;
 
-    /***operator=(const string& str)***/
-    std::cout<<"Test of operator=(char c)"<<std::endl;
-    K = e;
-    //  cout<<"e : " <<e.c_str()<<endl;
-    std::cout<<K.c_str()<<std::endl;
+  std::cout<<"Test operator + (const string&, const string&)"<<std::endl;
+  std::cout <<w+w<< std::endl;
 
+  cout<<"Test operator + (const string&, char)"<<endl;
+  char c = 'O' ;
+  cout<<w+c<<endl;
+
+
+
+  //TESTING OPERATORS +
+  std::cout<<"Test of operator = (const char*)"<<std::endl;
+  char const * k = "Hello world";
+  string K = k;
+  std::cout<<K.c_str()<<std::endl;
+
+  std::cout<<"Test of operator=(char c)"<<std::endl;
+  K = c;
+  std::cout<<K.c_str()<<std::endl;
+
+  std::cout<<"Test of operator=(char c)"<<std::endl;
+  K = e;
+  //  cout<<"e : " <<e.c_str()<<endl;
+  std::cout<<K.c_str()<<std::endl;
 
 
 
